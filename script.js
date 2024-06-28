5ab5d302454c6e934a54fc462fd9446d982c4fd1
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-28 13:18:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your JS code here. If required.
+const usernameInput=document.getElementById('username');
+const passwordInput=document.getElementById('password');
+const checkboxInput=document.getElementById('checkbox');
+const submitButton=document.getElementById('submit');
+const existingButton=document.getElementById('existing');
+
+function checkIfUserExistsInLocalStorage() {
+	if(localStorage.getItem('user')){
+		existingButton.style.display='block'
+	}else{
+		existingButton.style.display='none'
+	}
+}
+   
+checkIfUserExistsInLocalStorage()
+
+function saveUserInLocalStorage() {
+	const user={
+		username:usernameInput.value,
+		password:passwordInput.value,
+	}
+
+	localStorage.setItem('user',JSON.stringify(user))
+}
+ 
+function removeUserFromLocalStorage() {
+	localStorage.removeItem('user')
+}
+
+existingButton.addEventListener('click',(e)=>{
+	e.preventDefault();
+	alert('Logged in as .')
+})
+
+submitButton.addEventListener('click',(e)=>{
+	e.preventDefault();
+
+	if(!checkboxInput.checked){
+		removeUserFromLocalStorage()
+		// remove existing button
+		checkIfUserExistsInLocalStorage()
+		
+	}else{
+		saveUserInLocalStorage()
+		// show existing button
+		checkIfUserExistsInLocalStorage()
+	}
+	alert('Logged in as .')
+})
